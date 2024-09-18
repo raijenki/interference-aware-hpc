5c3b9624ab23c010c209e76fe51449904158df03
Daniel Medeiros <daniel@kuriate.com.br>
2024-09-18 14:18:31 +0200

    fix click options, rapl

--- launcher.py
+++ launcher.py
@@ -68,17 +68,17 @@ def app_run(app):
             sys.exit(1)
 
 def read_energy_socket(socket_id):
-    f = open(f"/sys/class/powercap/intel-rapl/intel-rapl\:{socket_id}/energy_uj", "r")
+    f = open(f"/sys/class/powercap/intel-rapl/intel-rapl:{socket_id}/energy_uj", "r")
     return f.read()
     
 
 @click.command()
 @click.option('--ncpus', required=True, default=0, help='Number of CPUs to be allocated (cgroupv2)')
-@click.option('--cpubind/--no-cpu-bind', required=True, help='Which CPU(s) the application should be executed (numactl).')
+@click.option('--cpubind', required=True, help='Which CPU(s) the application should be executed (numactl).')
 @click.option('--memory', required=True, default="1G", help='Allocate amount of memory to the application. (cgroupv2)')
 @click.option('--app', required=True, prompt='Application', help='The application file you wish to execute.')
 @click.option('--disk', type=(str, int), required=False, help='Disk bandwidth (device, amount of MB/s).')
-@click.option('--cpufreq/--no-cpufreq', prompt='Frequency for CPU')    
+@click.option('--cpufreq', prompt='Frequency for CPU')    
 @click.option('--logging/--no-logging', default=False, help='If you want to log the results or not.')
 @click.option('--rapl/--no-rapl', default=False, help='Measure energy using RAPL.')
 
@@ -91,7 +91,7 @@ def run(ncpus, cpubind, memory, app, disk, cpufreq, logging, rapl):
     sanity_check()
 
     # Check if applications are compiled 
-    app = app_run(app)
+    app_path = app_run(app)
 
     if cpufreq:
         # Set governor to userspace, set frequencies
@@ -134,9 +134,9 @@ def run(ncpus, cpubind, memory, app, disk, cpufreq, logging, rapl):
     if cpubind:
         cgroups_command =  (f"echo {cpubind} > /sys/fs/cgroup/{CGROUP_NAME}/cpuset.cpus")
         os.system(cgroups_command)
-        app_command = f"numactl --physcpubind={cpubind} {app}"
+        app_command = f"numactl --physcpubind={cpubind} {app_path}"
     else:
-        app_command = app
+        app_command = app_path
     
     # Run application
     start = time.time()
@@ -153,7 +153,6 @@ def run(ncpus, cpubind, memory, app, disk, cpufreq, logging, rapl):
 
         # Put PID into the cgroups process
         cgroups_command = f"echo {pid} > /sys/fs/cgroup/{CGROUP_NAME}/{SUBGROUP_NAME}/cgroup.procs"
-        print(cgroups_command)
         os.system(cgroups_command)
 
         # Wait process to finish
